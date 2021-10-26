61c540b96743226313e98a5774d577cc2726a7df
nehatkkhan786 <33215298+nehatkkhan786@users.noreply.github.com>
2021-10-26 10:48:45 +0530

    landing page done

--- bitssatoshiproject/settings.py
+++ bitssatoshiproject/settings.py
@@ -130,3 +130,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
